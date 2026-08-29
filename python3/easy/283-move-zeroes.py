c96153a07a8627a2353a0058f854f61a06d67d99
tejb96 <tejpreetbal@gmail.com>
2026-08-29 12:16:41 -0600

    two pointer with swap

---
+++ python3/easy/283-move-zeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        write=0
+
+        for read in range(len(nums)):
+            if nums[read]!=0:
+                nums[write],nums[read]=nums[read],nums[write]
+                write+=1
