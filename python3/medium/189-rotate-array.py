f8decc140ac509c0f8132ac9e38ce7bd64dbb5ab
tejb96 <tejpreetbal@gmail.com>
2026-08-30 00:44:11 -0600

    cleaned up

--- python3/medium/189-rotate-array.py
+++ python3/medium/189-rotate-array.py
@@ -1,36 +1,33 @@
 class Solution:
     def rotate(self, nums: list[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        length=len(nums)
-        
-        if length==1 or k==length or (k>length and k%length==0):
+        n = len(nums)
+        k %= n
+
+        if k == 0:
             return
 
-        nums.reverse()
-        i=0
-        j=k-1
+        def reverse(i, j):
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
 
-        if k>length:
-            floor_div=k//length
-            j=k-(floor_div*length)-1
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
 
-        while i<j:
-            print(i,j)
-            nums[i],nums[j]=nums[j],nums[i]
-            i+=1
-            j-=1
+# # alternative method using slices (much cleaner)
+# class Solution:
+#     def rotate(self, nums: list[int], k: int) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         n = len(nums)
+#         k %= n
 
-        i=k
-        j=length-1
+#         if k == 0:
+#             return
 
-        if k>length:
-            floor_div=k//length
-            i=k-(floor_div*length)
-
-        while i<j:
-            nums[i],nums[j]=nums[j],nums[i]
-            i+=1
-            j-=1         
-
+#         nums.reverse()
+#         nums[:k] = reversed(nums[:k])
+#         nums[k:] = reversed(nums[k:])
